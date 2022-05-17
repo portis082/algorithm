f14df0c6ac1ef5686fba9d4e38b3acb85c57b361
portis082 <portis082@gmail.com>
2022-05-17 20:59:17 +0900

    Add P number 9012

---
+++ baekjoon9012.js
@@ -0,0 +1,87 @@
+const fs = require("fs");
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+const N = input.shift();
+const array = input;
+console.log(array)
+console.log(typeof N)
+
+for(let i=0; i<N; i++){
+    const stack = []
+    array[i]
+}
+// const [N, L, R] = input.shift().split(" ").map(Number);
+// const array = input.map((i) => {
+//     return i.split(" ").map(Number);
+// });
+
+// let result = 0;
+// let boolean = true;
+
+// while(boolean) {
+//     const visited = Array.from(Array(N), () => Array(N).fill(0));
+//     const unionSet = [];
+//     for (let i = 0; i < N; i++) {
+//         for (let j = 0; j < N; j++) {
+//             if(!visited[i][j]){
+//                 const union = bfs(i, j, visited);
+//                 if(union.length !== 0){
+//                     unionSet.push(union);
+//                 }
+//             }
+//         }
+//     }
+
+//     unionSet.map((union) => {
+//         let sum = 0;
+//         union.map((el) => {
+//             sum += array[el[0]][el[1]];
+//         })
+//         const average = Math.trunc(sum/union.length)
+//         union.map((el) => {
+//             array[el[0]][el[1]] = average;
+//         })
+//     })
+//     result++;
+//     if(unionSet.length === 0) {
+//         boolean = false;
+//     }
+// }
+
+
+
+// function bfs(y, x, visited) {
+//     const queue = [];
+//     const union = [];
+
+//     const dx = [0, 0, 1, -1];
+//     const dy = [1, -1, 0, 0];
+
+//     queue.push([y, x]);
+//     visited[y][x] = 1;
+
+//     while (queue.length) {
+//         const current = queue.shift();
+//         const currentValue = array[current[0]][current[1]];
+
+//         for (let i = 0; i < 4; i++) {
+//             const next = [current[0] + dx[i], current[1] + dy[i]];
+
+//             if (0 <= next[0] && next[0] < N && 0 <= next[1] && next[1] < N && !visited[next[0]][next[1]]) {
+//                 if(Math.abs(currentValue-array[next[0]][next[1]]) >= L && Math.abs(currentValue-array[next[0]][next[1]]) <= R) {
+//                     if(!union.includes(next)) {
+//                         union.push(next);
+//                     }
+//                     if(!union.includes(current)) {
+//                         union.push(current);
+//                     }
+//                     visited[next[0]][next[1]] = 1;
+//                     queue.push(next);
+//                 }
+//             }
+//         }
+//     }
+//     return union;
+// }
+
+// console.log(result-1)
