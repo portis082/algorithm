40778415d93b9d103c1211f75179c8de2634a058
portis082 <portis082@gmail.com>
2022-05-25 16:22:27 +0900

    Update P number 2512

--- baekjoon2512.js
+++ baekjoon2512.js
@@ -3,13 +3,42 @@ const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
 input.shift();
 
-const array = input.shift().split(" ").map((el) => Number(el));
-const total = input.shift();
+const numberSet = input.shift().split(" ").map((el) => Number(el));
+const totalSum = input.shift();
 
-const b = array.reduce((sum, el) => {
-    return sum += el;
-})
-const a = Math.max(...array);
 
-console.log(b)
-console.log(total)
+const calculator = (array, total) => {
+    let max = Math.max(...array);
+    let min = 0;
+    let mid = Math.ceil((max + min)/2);
+    let result = 0;
+
+    let sum = array.reduce((pre, el) => {
+        return pre += el;
+    }, 0)
+
+    if(sum < total) {
+        return max;
+    }
+
+    while(max > min && max !== min + 1) {
+        mid = Math.ceil((max + min)/2);
+        sum = array.reduce((pre, el) => {
+            if(el <= mid) {
+                return pre += el;
+            } else {
+                return pre += mid;
+            }
+        }, 0);
+
+        if(sum <= total) {
+            min = mid;
+            result = mid;
+        } else {
+            max = mid;
+        }
+    }
+    console.log(result);
+}
+
+calculator(numberSet, totalSum);
