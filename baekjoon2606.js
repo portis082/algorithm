5bd1170a14ba98a02f00e1eb1cb5f3c92423b4ca
portis082 <portis082@gmail.com>
2022-05-26 22:59:23 +0900

    Update P number 2606

--- baekjoon2606.js
+++ baekjoon2606.js
@@ -2,6 +2,23 @@ const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
 input.shift();
+input.shift();
 
-const numberSet = input.shift().split(" ").map((el) => Number(el));
-const totalSum = input.shift();
+const linkSet = input.map((el) => {
+    return el.split(" ").map(Number);
+})
+
+console.log(linkSet)
+
+// const stack = [1];
+
+// linkSet.map((el) => {
+//     if(stack.includes(el[0]) && !(stack.includes(el[1]))) {
+//         stack.push(el[1]);
+//     }
+//     if(!(stack.includes(el[0])) && stack.includes(el[1])) {
+//         stack.push(el[0]);
+//     }
+// })
+
+// console.log(stack.length - 1);
