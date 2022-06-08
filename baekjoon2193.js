e5133335cad73a57e20f241344507a8fdc908b6d
portis082 <portis082@gmail.com>
2022-06-09 00:48:15 +0900

    Solved P number 2193

--- baekjoon2193.js
+++ baekjoon2193.js
@@ -1,28 +1,41 @@
 const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
-
-
 let N = Number(input[0]);
-let num = 0;
-let count = 0;
 
-let queue = [[1, 1]];
+// let queue = [[1, 1]];
 
-for(let i=0; i<N ;i++) {
-    const newQueue = [];
-    while(queue.length !== 0){
-        const current = queue.shift();
-        count++;
-        if(current[1] === 1){
-            newQueue.push([current[0]*10, 0]);
-        } else {
-            newQueue.push([current[0]*10, 0]);
-            newQueue.push([current[0]*10 + 1, 1]);
-        }
+// for(let i=0; i<N-1 ;i++) {
+//     const newQueue = [];
+
+//     while(queue.length !== 0){
+//         const current = queue.shift();
+
+//         if(current[1] === 1){
+//             newQueue.push([current[0]*10, 0]);
+//         } else {
+//             newQueue.push([current[0]*10, 0]);
+//             newQueue.push([current[0]*10 + 1, 1]);
+//         }
+//     }
+//     queue = newQueue;
+// }
+
+// console.log(queue.length);
+
+const calculator = (n) => {
+    const result = [BigInt(1), BigInt(1)];
+
+    if(n < 3) {
+        return 1;
     }
-    console.log(newQueue);
-    queue = newQueue;
+    
+    for(let i=0; i<n-2; i++){
+        result.push(BigInt(result[i] + result[i+1]));
+    }
+
+    console.log(result)
+    return result[result.length - 1];
 }
 
-console.log(count);
+console.log(String(calculator(N)));
