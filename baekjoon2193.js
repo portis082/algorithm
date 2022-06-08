2d6c77a412c9d89f68a41e2f7d8cb63c32cef7de
portis082 <portis082@gmail.com>
2022-06-08 23:57:04 +0900

    Update P number 2193

--- baekjoon2193.js
+++ baekjoon2193.js
@@ -1,2 +1,28 @@
 const fs = require("fs");
-const array = fs.readFileSync("test.txt").toString().trim().split("\n");
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+
+
+let N = Number(input[0]);
+let num = 0;
+let count = 0;
+
+let queue = [[1, 1]];
+
+for(let i=0; i<N ;i++) {
+    const newQueue = [];
+    while(queue.length !== 0){
+        const current = queue.shift();
+        count++;
+        if(current[1] === 1){
+            newQueue.push([current[0]*10, 0]);
+        } else {
+            newQueue.push([current[0]*10, 0]);
+            newQueue.push([current[0]*10 + 1, 1]);
+        }
+    }
+    console.log(newQueue);
+    queue = newQueue;
+}
+
+console.log(count);
