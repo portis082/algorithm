271b742bc0f3712bf95088f959f37eb41e1826e3
portis082 <portis082@gmail.com>
2022-06-10 23:13:06 +0900

    Add P number 1991

--- baekjoon2193.js
+++ baekjoon2193.js
@@ -3,26 +3,6 @@ const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
 let N = Number(input[0]);
 
-// let queue = [[1, 1]];
-
-// for(let i=0; i<N-1 ;i++) {
-//     const newQueue = [];
-
-//     while(queue.length !== 0){
-//         const current = queue.shift();
-
-//         if(current[1] === 1){
-//             newQueue.push([current[0]*10, 0]);
-//         } else {
-//             newQueue.push([current[0]*10, 0]);
-//             newQueue.push([current[0]*10 + 1, 1]);
-//         }
-//     }
-//     queue = newQueue;
-// }
-
-// console.log(queue.length);
-
 const calculator = (n) => {
     const result = [BigInt(1), BigInt(1)];
 
