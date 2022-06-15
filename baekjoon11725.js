996a95d54a306c34788028053b02f85a54dee09b
portis082 <portis082@gmail.com>
2022-06-15 23:47:46 +0900

    Update P number 11725 and 1463

--- baekjoon11725.js
+++ baekjoon11725.js
@@ -1,2 +1,32 @@
 const fs = require("fs");
-const array = fs.readFileSync("test.txt").toString().trim().split("\n");
+let input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+const N = input.shift();
+
+input = input.map((el) => {
+    return el.split(" ").map(Number);
+});
+
+const result = {};
+const queue = [1];
+const visited = [];
+
+while(queue.length>0) {
+    const current = queue.shift();
+    visited.push(current);
+
+    array.map((el) => {
+        if(el[0] === current && !visited.includes(el[1])) {
+            queue.push(el[1]);
+            result[el[1]] = el[0];
+        }
+        if(el[1] === current && !visited.includes(el[0])) {
+            queue.push(el[0]);
+            result[el[0]] = el[1];
+        }
+    });
+}
+
+for(let i=2; i<=N; i++){
+    console.log(result[i]);
+}
--- baekjoon1463.js
+++ baekjoon1463.js
@@ -1,5 +1,21 @@
 const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
-const N = Number(input[0]);
-console.log(input)
+let n = Number(input[0]);
+let count = 0;
+
+while(n !== 1) {
+    console.log(n);
+    if(n%3 === 0){
+        n = n/3;
+    } else if(n%3 === 1 && n%2 !== 0 || n%3 === 1 && ((n-1)%3)%3 === 0) {
+        n = n-1;       
+    } else if(n%2 === 0) {
+        n = n/2;
+    } else {
+        n = n-1;
+    }
+    count++;
+}
+
+console.log(count);
