5a8cc5f1482041a90a5c97866d52696444a023a3
portis082 <portis082@gmail.com>
2022-06-16 23:41:29 +0900

    Solved P number 10845 and 10828

--- baekjoon10828.js
+++ baekjoon10828.js
@@ -1,5 +1,31 @@
 const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
-const N = input.shift();
-const array = input;
+const N = Number(input[0])
+const stack = [];
+
+let count = 0;
+let head = 0;
+let answer = "";
+
+for(let i=1; i<N+1; i++) {
+    if(input[i].includes("push")) {
+        stack.push(Number(input[i].split(" ")[1]));
+        count++;
+    } else if(input[i] === "size") {
+        answer += count + "\n";
+    } else if(input[i] === "empty") {
+        answer += Number(count === 0) + "\n"
+    } else if(count === 0) {
+        answer += -1 + "\n"
+    } else if(input[i] === "front")  {
+        answer += stack[head] + "\n"
+    } else if(input[i] === "back") {
+        answer += stack[head+count-1] + "\n"
+    } else if(input[i] === "pop") {
+        answer += stack[head] + "\n"
+        head++;
+        count--;
+    }
+}
+console.log(answer.slice(0, -1));
--- baekjoon10845.js
+++ baekjoon10845.js
@@ -1,21 +1,27 @@
 const fs = require("fs");
-const array = fs.readFileSync("test.txt").toString().trim().split("\n");
-const stack = [];
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
-for(let i=1; i<array.length; i++) {
-    if(array[i].includes("push")) {
-        stack.push(array[i].split(" ")[1]);
-    } else if(array[i] === "size") {
-        console.log(stack.length);
-    } else if(array[i] === "empty") {
-        console.log(Number(stack.length === 0));
-    } else if(stack.length === 0) {
-        console.log(-1);
-    } else if(array[i] === "front")  {
-        console.log(stack[0]);
-    } else if(array[i] === "back") {
-        console.log(stack[stack.length - 1]);
-    } else if(array[i] === "pop") {
-        console.log(stack.shift());
+const N = Number(input[0])
+const queue = [];
+
+let count = 0;
+let answer = "";
+
+for(let i=1; i<N+1; i++) {
+    if(input[i].includes("push")) {
+        queue.push(Number(input[i].split(" ")[1]));
+        count++;
+    } else if(input[i] === "size") {
+        answer += count + "\n";
+    } else if(input[i] === "empty") {
+        answer += Number(count === 0) + "\n";
+    } else if(count === 0) {
+        answer += -1 + "\n";
+    } else if(input[i] === "top") {
+        answer += queue[count-1] + "\n";
+    } else if(input[i] === "pop") {
+        answer += queue.pop() + "\n";
+        count--;
     }
 }
+console.log(answer.slice(0, -1));
