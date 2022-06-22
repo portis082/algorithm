656216ce347ff6a52c1d05d1b1f549a7b535120e
portis082 <portis082@gmail.com>
2022-06-22 23:06:57 +0900

    Solved P number 1991

--- baekjoon1991.js
+++ baekjoon1991.js
@@ -1,2 +1,36 @@
 const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+input.shift();
+
+let preorderResult = "";
+let inorderResult = "";
+let postorderResult = "";
+
+const array = input.map((el) => {
+    return el.split(" ").map((char) => {
+        if(char !== "."){
+            return char.charCodeAt() - 65;
+        } else {
+            return -1;
+        }
+    });
+}).sort((a, b) => {
+    return a[0] - b[0]
+});
+
+order(0);
+console.log(preorderResult);
+console.log(inorderResult);
+console.log(postorderResult);
+
+function order(root) {
+    preorderResult += String.fromCharCode(root + 65);
+    if(array[root][1] !== -1) {
+        order(array[root][1]);
+    }
+    inorderResult += String.fromCharCode(root + 65);
+    if(array[root][2] !== -1) {
+        order(array[root][2]);
+    }
+    postorderResult += String.fromCharCode(root + 65);
+}
