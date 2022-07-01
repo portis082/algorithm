47dc1166f59c38486dbd1325ae089259cc06ab1b
portis082 <portis082@gmail.com>
2022-07-01 15:13:18 +0900

    Solved P number 9663

--- baekjoon9663.js
+++ baekjoon9663.js
@@ -2,14 +2,11 @@ const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
 const N = Number(input[0]);
-const array = Array(N).fill(Array(N).fill(true));
 const axisArray = [];
 let count = 0;
-console.log(array);
-
 
 recursion(axisArray, N);
-
+console.log(count);
 
 function recursion(array, n) {
     if(n === 0) {
@@ -18,20 +15,30 @@ function recursion(array, n) {
     }
 
     for(let i=0; i<N; i++) {
-        if(array.length === 0) {
-            array.push(i);
-            recursion(array, n-1);
+        const newArray = [...array];
+        if(newArray.length === 0) {
+            newArray.push(i);
+            recursion(newArray, n-1);
         } else {
-            // for(let j=0; j<array.length; j++) {
-            //     array[j] !== i && array.length - i !== 
-            // }
+            let boolean = true;
+            for(let j=0; j<newArray.length; j++) {
+                if(newArray[j] === i) {
+                    boolean = false;
+                    break;
+                }
+                if(newArray.length - j === newArray[j] - i) {
+                    boolean = false;
+                    break;
+                }
+                if(newArray.length - j === i - newArray[j]) {
+                    boolean = false;
+                    break;
+                }
+            }
+            if(boolean) {
+                newArray.push(i);
+                recursion(newArray, n-1);
+            }
         }
-        // if() {
-
-        // }
-        let boolean 
-        recursion(array, n-1);
     }
 }
-
-console.log(count);
