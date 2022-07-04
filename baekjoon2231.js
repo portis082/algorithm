76cb74eaf015b5537e7ecc85514a57d5ab0464bf
portis082 <portis082@gmail.com>
2022-07-04 21:15:26 +0900

    Add and Solved P number 2231

---
+++ baekjoon2231.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+const N = Number(input[0]);
+let result = 0;
+
+for(let i=0; i<N; i++){
+    let sum = 0;
+
+    sum += i;
+    String(i).split("").forEach((el) => {
+        sum += Number(el);
+    });
+
+    if(sum === N){
+        result = i;
+        break;
+    }
+}
+console.log(result)
