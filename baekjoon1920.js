aa97a17cfd635e27670035dfd495f569723bb32f
portis082 <portis082@gmail.com>
2022-07-06 15:04:15 +0900

    Solved P num 1920

--- baekjoon1920.js
+++ baekjoon1920.js
@@ -1,11 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
-input.shift();
-const array = input.shift().split(" ");
-input.shift();
-const target = input.shift().split(" ");
+const N = Number(input[0]);
+const M = Number(input[2]);
+const array = input[1].split(" ").map(Number).sort((a, b) => {
+    return a-b;
+});
+const target = input[3].split(" ").map(Number);
+const result = Array(M).fill(0);
 
-target.map((el) => {
-    console.log(Number(array.includes(el)));
-})
+for(let i=0; i<M; i++) {
+    let start = 0;
+    let end = N-1;
+    let mid = Math.trunc((start+end)/2);
+
+    while(end >= start) {
+        if(array[mid] === target[i]) {
+            result[i] = 1;
+            break;
+        } else if(array[mid] > target[i]) {
+            end = mid - 1;
+        } else if(array[mid] < target[i]) {
+            start = mid + 1;
+        }
+        mid = Math.trunc((start+end)/2);
+    }
+}
+
+console.log(result.join("\n"));
